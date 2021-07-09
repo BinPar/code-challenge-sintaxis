46a07674ee7561092eaf110f497fc8fce7b8b142
Miguel Rodríguez <miguel@binpar.com>
2021-07-09 16:57:49 +0200

    solved

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,33 @@
 import samples from './samples';
 
 const isCorrectSample = (sample: string): boolean => {
-    
+  let isCorrect = true;
+  const openChars: string[] = [];
+  for (let i = 0, l = sample.length; i < l && isCorrect; i++) {
+    const char = sample[i];
+    const [currentOpenChar] = openChars;
+    if (char === ')') {
+      if (currentOpenChar !== '(') {
+        isCorrect = false;
+      } else {
+        openChars.shift();
+      }
+    }
+    if (char === ']') {
+      if (currentOpenChar !== '[') {
+        isCorrect = false;
+      } else {
+        openChars.shift();
+      }
+    }
+    if (char === '(') {
+      openChars.unshift(char);
+    }
+    if (char === '[') {
+      openChars.unshift(char);
+    }
+  }
+  return isCorrect && !openChars.length;
 };
 
 const result = samples.map(sample => ({
